dcb8977e26edf91c440fc76895c78c2104d17020
mym0404 <mym0404@gmail.com>
2022-02-28 00:46:38 +0900

    uglify

--- src/PhoneNumberTextModule.ts
+++ src/PhoneNumberTextModule.ts
@@ -4,44 +4,41 @@
  * @param arg2 changed raw string for phone number
  * @return valid phone number formmated with hyphen pattern e.g.) 010-0000-0000
  * @author you
+ * @link https://github.com/reactjs/reactjs.org/issues/3896
  * */
-export default function _______EDIT_________THIS_______ONLY__________(
+export default function __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED(
   arg1: string,
   arg2: string,
 ): string {
-  if (arg2.length === arg1.length + 1) {
+  const getStringLength = (str: string) => str.length;
+
+  let phoneNumberFormatted = '';
+
+  if (getStringLength(arg2) === getStringLength(arg1) + 1) {
     // added
 
     // no need change
-    if (arg2.length === 13) {
-      return arg2;
+    if (getStringLength(arg2) === 13) {
+      phoneNumberFormatted = arg2;
+    } else if (getStringLength(arg2) > 13) {
+      phoneNumberFormatted = arg1;
+    } else if ([1, 2, 5, 6, 7, 10, 11, 12].includes(getStringLength(arg2))) {
+      // list 1,2,5,6,7,10,11,12 means no need hyphen appended
+      phoneNumberFormatted = arg2;
+    } else if ([3, 8].includes(getStringLength(arg2))) {
+      // list 3,8 means it needs hyphen appended
+      phoneNumberFormatted = arg2 + '-';
     }
-
-    // upper length
-    if (arg2.length > 13) {
-      return arg1;
-    }
-
-    // list 1,2,5,6,7,10,11,12 means no need hyphen appended
-    if ([1, 2, 5, 6, 7, 10, 11, 12].includes(arg2.length)) {
-      return arg2;
-    }
-    // list 3,8 means it needs hyphen appended
-    if ([3, 8].includes(arg2.length)) {
-      return arg2 + '-';
-    }
-  } else if (arg2.length === arg1.length - 1) {
+  } else if (getStringLength(arg2) === getStringLength(arg1) - 1) {
     // removed
-    if (arg2.length === 0) {
-      return '';
+    if (getStringLength(arg2) === 0) {
+      phoneNumberFormatted = '';
+    } else if ([4, 9].includes(getStringLength(arg1))) {
+      phoneNumberFormatted = arg2.substring(0, getStringLength(arg2) - 1);
+    } else {
+      phoneNumberFormatted = arg2;
     }
-
-    if ([4, 9].includes(arg1.length)) {
-      return arg2.substring(0, arg2.length - 1);
-    }
-
-    return arg2;
   }
 
-  return '';
+  return phoneNumberFormatted;
 }
